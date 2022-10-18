019d6ce4ee58d991f0a0af61469af2cb4e7055e8
Yuriy <gamecraftsteam@gmail.com>
2022-10-18 19:07:02 +0300

    Initial Commit

---
+++ cryptoMonitoring/JSONDecode.swift
@@ -0,0 +1,37 @@
+//
+//  JSONDecode.swift
+//  cryptoMonitoring
+//
+//  Created by Юрий on 03.10.2022.
+//
+
+import Foundation
+
+let urlString = "https://rest.coinapi.io/v1/assets?apikey=87F2B9A6-992C-4212-ADAC-190444275DA9"
+var resultsReturn = [Coin]()
+
+// Make timer for update price every 10s, timer ready but cells update only when invisible
+// Save fav on phone
+// Update Button
+
+func getData() {
+    let url = URL(string: urlString)
+    if let url = url {
+        URLSession.shared.dataTask(with: url) {data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let results = try decoder.decode([Coin].self, from: data)
+                resultsReturn = results
+                print("in JSONDecode: \(resultsReturn.count)")
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+}
+
